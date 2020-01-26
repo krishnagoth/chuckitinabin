817635d676027a8b08cfb9d152b19ad1b6d4c34f
krighnagoth <endarkened@gmail.com>
2020-01-27 08:56:57 +1100

    Added authz

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,20 @@
 import { GeoJsonObject } from 'geojson';
 
 export class RubbishLocation {
-  constructor(readonly geojson: GeoJsonObject, readonly log: Log = [], readonly id?: string) { }
+  constructor(
+    readonly geojson: GeoJsonObject,
+    readonly log: Log = [],
+    readonly id?: string
+  ) {}
 }
 
 export class RubbishLocationDbEntry extends RubbishLocation {
-  constructor(readonly _id: string, geojson: GeoJsonObject, log: Log, id?: string) {
+  constructor(
+    readonly _id: string,
+    geojson: GeoJsonObject,
+    log: Log,
+    id?: string
+  ) {
     super(geojson, log, id);
   }
 }
@@ -13,20 +22,20 @@ export class RubbishLocationDbEntry extends RubbishLocation {
 export type Log = Array<LogEntry>;
 
 export class LogEntry {
-  constructor(readonly description: string) { }
+  constructor(readonly description: string) {}
 }
 
 export namespace ApiOps {
   export class Result<T extends ResultTypes> {
-    constructor(readonly data: T) { }
+    constructor(readonly data: T) {}
 
     toString(): string {
       return JSON.stringify(this);
     }
   }
   export type RubbishLocationId = {
-    id: string
-  }
+    id: string;
+  };
   export type RubbishLocations = Array<RubbishLocation>;
   type ResultTypes = RubbishLocationId | RubbishLocations | RubbishLocation;
 }
--- src/resources/map.ts
+++ src/resources/map.ts
@@ -1,7 +1,5 @@
 import { AxiosResponse, default as axios } from 'axios';
-import createAuth0Client from '@auth0/auth0-spa-js';
 import { ApiOps, LogEntry, RubbishLocation } from '../common';
-import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
 
 let map: google.maps.Map;
 
@@ -12,24 +10,28 @@ function initMap() {
   });
 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      map.setCenter(pos);
-    }, function () { });
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+      },
+      function() {}
+    );
   }
 
-  map.addListener('tilesloaded', function () {
+  map.addListener('tilesloaded', function() {
     var bounds: google.maps.LatLngBounds = map.getBounds();
     loadLocations(map);
   });
-  map.data.addListener('click', function (dataClick: google.maps.Data.MouseEvent) {
-
+  map.data.addListener('click', function(
+    dataClick: google.maps.Data.MouseEvent
+  ) {
     var div = document.createElement('div');
 
-    attachLog(dataClick.feature.getId(), function () {
+    attachLog(dataClick.feature.getId(), function() {
       for (var logEntry of dataClick.feature.getProperty('log')) {
         var entrySpan = document.createElement('span');
         entrySpan.innerHTML = `Description: ${logEntry.description}`;
@@ -42,7 +44,7 @@ function initMap() {
       position: dataClick.latLng
     });
     popup.open(map);
-    map.data.addListener('click', function (dataClick) {
+    map.data.addListener('click', function(dataClick) {
       popup.close();
     });
   });
@@ -51,31 +53,40 @@ function initMap() {
 function addLocation() {
   const button = this as HTMLButtonElement;
   button.setAttribute('style', 'display: none');
-  var drawingManager: google.maps.drawing.DrawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.POLYGON,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+  var drawingManager: google.maps.drawing.DrawingManager = new google.maps.drawing.DrawingManager(
+    {
+      drawingMode: google.maps.drawing.OverlayType.POLYGON,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+      }
     }
-  });
+  );
 
   var cancelBtn = document.getElementById('cancel-edit-btn');
   cancelBtn.setAttribute('style', '');
-  cancelBtn.onclick = function () {
+  cancelBtn.onclick = function() {
     drawingManager.setMap(null);
     button.setAttribute('style', '');
     cancelBtn.setAttribute('style', 'display: none');
-  }
+  };
 
   drawingManager.setMap(map);
-  google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event: google.maps.drawing.OverlayCompleteEvent) {
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(
+    event: google.maps.drawing.OverlayCompleteEvent
+  ) {
     if (event.type === google.maps.drawing.OverlayType.POLYGON) {
-
       const overlay: google.maps.Polygon = event.overlay as google.maps.Polygon;
 
       var locationFeature: google.maps.Data.Feature;
-      console.log(`Overlay coords ${JSON.stringify(overlay.getPath().getArray(), null, 2)}`);
+      console.log(
+        `Overlay coords ${JSON.stringify(
+          overlay.getPath().getArray(),
+          null,
+          2
+        )}`
+      );
       if (event.type === google.maps.drawing.OverlayType.POLYGON) {
         locationFeature = new google.maps.Data.Feature({
           geometry: new google.maps.Data.Polygon([overlay.getPath().getArray()])
@@ -84,37 +95,54 @@ function addLocation() {
 
       var formDiv = document.createElement('div');
       formDiv.setAttribute('id', 'edit-location-dialogue');
-      formDiv.innerHTML = "<form onsubmit='saveLocation(this); return false;' action='/api/locations'><input type='text' placeholder='Entry description' name='logEntry', id='location-log-entry' /><input type='hidden' id='location-geojson' name='geojson' /><button type='submit'>Submit</button></form>";
+      formDiv.innerHTML =
+        "<form onsubmit='saveLocation(this); return false;' action='/api/locations'><input type='text' placeholder='Entry description' name='logEntry', id='location-log-entry' /><input type='hidden' id='location-geojson' name='geojson' /><button type='submit'>Submit</button></form>";
 
       var popup = new google.maps.InfoWindow({
         content: formDiv,
-        position: overlay.getPath().getArray()[overlay.getPath().getArray().length - 1]
+        position: overlay.getPath().getArray()[
+          overlay.getPath().getArray().length - 1
+        ]
       });
 
-      cancelBtn.onclick = function (this: HTMLElement, ev: MouseEvent) {
+      cancelBtn.onclick = function(this: HTMLElement, ev: MouseEvent) {
         popup.close();
         map.data.remove(locationFeature);
         event.overlay.setVisible(false);
         drawingManager.setMap(null);
         button.setAttribute('style', '');
         cancelBtn.setAttribute('style', 'display: none');
-      }
+      };
 
-      popup.addListener('domready', function (ev) {
-        locationFeature.toGeoJson(function (geojson) {
-          document.getElementById('location-geojson').setAttribute('value', JSON.stringify(geojson));
+      popup.addListener('domready', function(ev) {
+        locationFeature.toGeoJson(function(geojson) {
+          document
+            .getElementById('location-geojson')
+            .setAttribute('value', JSON.stringify(geojson));
         });
-        document.getElementById('edit-location-dialogue').getElementsByTagName('form')[0].addEventListener('change', function (e) {
-          locationFeature.setProperty('log', [{ description: document.getElementById('location-log-entry').getAttribute('value') }]);
-        });
-        document.getElementById('edit-location-dialogue').getElementsByTagName('form')[0].addEventListener('submit', function (e) {
-          popup.close();
-          event.overlay.setVisible(false);
-          button.setAttribute('style', '');
-          cancelBtn.setAttribute('style', 'display: none');
-          map.data.add(locationFeature);
-          drawingManager.setMap(null);
-        });
+        document
+          .getElementById('edit-location-dialogue')
+          .getElementsByTagName('form')[0]
+          .addEventListener('change', function(e) {
+            locationFeature.setProperty('log', [
+              {
+                description: document
+                  .getElementById('location-log-entry')
+                  .getAttribute('value')
+              }
+            ]);
+          });
+        document
+          .getElementById('edit-location-dialogue')
+          .getElementsByTagName('form')[0]
+          .addEventListener('submit', function(e) {
+            popup.close();
+            event.overlay.setVisible(false);
+            button.setAttribute('style', '');
+            cancelBtn.setAttribute('style', 'display: none');
+            map.data.add(locationFeature);
+            drawingManager.setMap(null);
+          });
       });
       popup.open(map);
     }
@@ -123,17 +151,18 @@ function addLocation() {
 
 function saveLocation(form: HTMLFormElement) {
   var fd = new FormData(form);
-  axios.post<ApiOps.Result<ApiOps.RubbishLocationId>>(
-    form.action,
-    new RubbishLocation(
-      JSON.parse(fd.get('geojson').toString()),
-      [new LogEntry(fd.get('logEntry').toString())]
+  axios
+    .post<ApiOps.Result<ApiOps.RubbishLocationId>>(
+      form.action,
+      new RubbishLocation(JSON.parse(fd.get('geojson').toString()), [
+        new LogEntry(fd.get('logEntry').toString())
+      ])
     )
-  ).then((res: AxiosResponse<ApiOps.Result<ApiOps.RubbishLocationId>>) => {
-    if (res.status === 200) {
-      const locationId = res.data.data.id;
-    }
-  });
+    .then((res: AxiosResponse<ApiOps.Result<ApiOps.RubbishLocationId>>) => {
+      if (res.status === 200) {
+        const locationId = res.data.data.id;
+      }
+    });
 }
 
 function loadLocations(map: google.maps.Map) {
@@ -144,22 +173,33 @@ function loadLocations(map: google.maps.Map) {
     }
   });
 
-  axios.post<ApiOps.Result<ApiOps.RubbishLocations>>(`/api/locations/search?bounds=${JSON.stringify(map.getBounds().toJSON())}&notIn=${JSON.stringify(featuresIds)}`)
+  axios
+    .post<ApiOps.Result<ApiOps.RubbishLocations>>(
+      `/api/locations/search?bounds=${JSON.stringify(
+        map.getBounds().toJSON()
+      )}&notIn=${JSON.stringify(featuresIds)}`
+    )
     .then((res: AxiosResponse<ApiOps.Result<ApiOps.RubbishLocations>>) => {
       if (res.status === 200) {
         for (var loc of res.data.data as Array<RubbishLocation>) {
           if (!map.data.getFeatureById(loc.id)) {
-            map.data.addGeoJson(loc.geojson).forEach((feature: google.maps.Data.Feature) => {
-              map.data.add(new google.maps.Data.Feature({
-                geometry: feature.getGeometry(),
-                id: loc.id,
-                properties: { log: loc.log }
-              }));
-            });
+            map.data
+              .addGeoJson(loc.geojson)
+              .forEach((feature: google.maps.Data.Feature) => {
+                map.data.add(
+                  new google.maps.Data.Feature({
+                    geometry: feature.getGeometry(),
+                    id: loc.id,
+                    properties: { log: loc.log }
+                  })
+                );
+              });
           }
         }
       } else {
-        throw new Error(`Got unexpected response ${JSON.stringify(res, null, 2)}`);
+        throw new Error(
+          `Got unexpected response ${JSON.stringify(res, null, 2)}`
+        );
       }
     })
     .catch((err: Error) => {
@@ -169,7 +209,8 @@ function loadLocations(map: google.maps.Map) {
 
 function attachLog(locationId: string | number, cb: () => void) {
   if (!map.data.getFeatureById(locationId).getProperty('log')) {
-    axios.get<ApiOps.Result<RubbishLocation>>(`/api/locations/${locationId}`)
+    axios
+      .get<ApiOps.Result<RubbishLocation>>(`/api/locations/${locationId}`)
       .then((res: AxiosResponse<ApiOps.Result<RubbishLocation>>) => {
         if (res.status === 200) {
           var location = res.data.data as RubbishLocation;
@@ -184,19 +225,24 @@ function attachLog(locationId: string | number, cb: () => void) {
 }
 
 window.addEventListener('load', () => {
-
   (<any>window).addLocation = addLocation;
   (<any>window).saveLocation = saveLocation;
 
   if (document.getElementById('login-btn')) {
-    document.getElementById('login-btn').addEventListener('click', () => axios.get('/login', {
-      withCredentials: true
-    }));
+    document.getElementById('login-btn').addEventListener('click', () =>
+      axios.get('/login', {
+        withCredentials: true
+      })
+    );
   }
   if (document.getElementById('logout-btn')) {
-    document.getElementById('logout-btn').addEventListener('click', () => axios.get('/logout'));
+    document
+      .getElementById('logout-btn')
+      .addEventListener('click', () => axios.get('/logout'));
   }
-  document.getElementById('add-location-btn').addEventListener('click', addLocation);
+  document
+    .getElementById('add-location-btn')
+    .addEventListener('click', addLocation);
 });
 
-(<any>window).initMap = initMap; 
+(<any>window).initMap = initMap;
